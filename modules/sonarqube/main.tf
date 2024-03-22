bf0b3c9fc35ba33f9d8e6e94cdd9bca9e2f3bf61
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-03-21 22:02:43 -0400

    send the load balancer information to ec2 instances

--- modules/sonarqube/main.tf
+++ modules/sonarqube/main.tf
@@ -50,3 +50,10 @@ resource "aws_lb_target_group" "tg" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
+
+
+resource "aws_lb_target_group_attachment" "attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.sonarqube.id
+  port             = var.port
+}
